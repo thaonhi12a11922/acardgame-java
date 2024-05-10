9ea649b13335bf4ecdf261a8ea2697dcb25912bb
tnganduong <thanhngan2332@gmail.com>
2024-05-10 08:44:38 +0700

    edit + add new function in PlayPage

--- A_Card_Game/Main.java
+++ A_Card_Game/Main.java
@@ -1,7 +1,12 @@
 package A_Card_Game;
 public class Main {
     public static void main(String[] args) throws Exception{
-        new Game();
+        // new Game();
+        try {
+            PlayPage.startGame();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
